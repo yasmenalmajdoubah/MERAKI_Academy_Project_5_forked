2aac0eb2c7f49a1bb3969c9b7d555d0a4283ae34
bakirhammad <bakir.hammad@gmail.com>
2024-02-10 22:05:16 +0300

    create server

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const cors = require("cors")
+require("dotenv").config()
+
+app.use(express.json())
+app.use(cors());
+
+
+// ===== Routers ==========
+
+
+
+
+//=========================
+
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, ()=>{
+ console.log(`Server run on http://localhost${PORT}`);   
+});
