50310212dc116881c5faf73bc081d6fe37364cd5
HamzaFAqel <hamzah.f.aqel@gmail.com>
2024-02-12 00:19:36 +0300

    route delete Like

--- backend/route/post.js
+++ backend/route/post.js
@@ -1,5 +1,5 @@
 const express=require("express")
-const { createNewPost, getPostsByUser, getPostsByField, updatePostById, deletePostById, addLike, getLikesByPost } = require("../controllers/post")
+const { createNewPost, getPostsByUser, getPostsByField, updatePostById, deletePostById, addLike, getLikesByPost, deleteLike } = require("../controllers/post")
 const authentication=require("../middlewares/authentication")
 const authorization=require("../middlewares/authorization")
 const postRouter=express.Router()
@@ -13,6 +13,6 @@ postRouter.put("/post/:post_id",authentication,updatePostById)
 postRouter.delete("/post/:post_id",authentication,deletePostById)
 postRouter.post("/likes/:post_id",authentication,addLike)
 postRouter.get("/likes/:post_id",authentication,getLikesByPost)
-
+postRouter.delete("/likes/:like_id",authentication,deleteLike)
 
 module.exports=postRouter
