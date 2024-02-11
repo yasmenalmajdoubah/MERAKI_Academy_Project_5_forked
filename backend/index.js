6061bfc6b650b5e1eb33136e0a5363c1d5ea5420
bakirhammad <bakir.hammad@gmail.com>
2024-02-11 10:17:35 +0300

    solve connection error

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,18 @@
 const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 require("./models/db");
+
 const app = express();
-const cors = require("cors")
-require("dotenv").config()
 
-app.use(express.json())
+app.use(express.json());
 app.use(cors());
-
 
 // ===== Routers ==========
 
-
-
-
 //=========================
 
-
-const PORT = process.env.PORT || 5000
-app.listen(PORT, ()=>{
- console.log(`Server run on http://localhost${PORT}`);   
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server run on http://localhost${PORT}`);
 });
