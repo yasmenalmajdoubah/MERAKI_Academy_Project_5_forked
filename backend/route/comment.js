a078fa8d312f39bfd269e0a3451a3eaf450dcdd3
HamzaFAqel <hamzah.f.aqel@gmail.com>
2024-02-12 22:15:29 +0300

    route get comments by post

--- backend/route/comment.js
+++ backend/route/comment.js
@@ -1,8 +1,8 @@
 const express=require("express")
 const authentication = require("../middlewares/authentication")
-const { createNewComment } = require("../controllers/comment")
+const { createNewComment, getCommentsByPost } = require("../controllers/comment")
 const commentRouter=express.Router()
 
 commentRouter.post("/comment/:post_id",authentication,createNewComment)
-
+commentRouter.get("/comment/:post_id",authentication,getCommentsByPost)
 module.exports=commentRouter
