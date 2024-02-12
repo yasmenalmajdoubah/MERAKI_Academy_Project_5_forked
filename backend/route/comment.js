461a56feed4a6db549f4ad2ab4e26801a2690f62
HamzaFAqel <hamzah.f.aqel@gmail.com>
2024-02-12 22:26:00 +0300

    route update comment by id

--- backend/route/comment.js
+++ backend/route/comment.js
@@ -1,8 +1,9 @@
 const express=require("express")
 const authentication = require("../middlewares/authentication")
-const { createNewComment, getCommentsByPost } = require("../controllers/comment")
+const { createNewComment, getCommentsByPost, updateCommentById } = require("../controllers/comment")
 const commentRouter=express.Router()
 
 commentRouter.post("/comment/:post_id",authentication,createNewComment)
 commentRouter.get("/comment/:post_id",authentication,getCommentsByPost)
+commentRouter.put("/comment/:comment_id",authentication,updateCommentById)
 module.exports=commentRouter
