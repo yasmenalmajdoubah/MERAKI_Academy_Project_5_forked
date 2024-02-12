5c94ad35e83c94df8910b6d289a093c54b973678
HamzaFAqel <hamzah.f.aqel@gmail.com>
2024-02-12 22:44:37 +0300

    route delete comment by id

--- backend/route/comment.js
+++ backend/route/comment.js
@@ -1,9 +1,10 @@
 const express=require("express")
 const authentication = require("../middlewares/authentication")
-const { createNewComment, getCommentsByPost, updateCommentById } = require("../controllers/comment")
+const { createNewComment, getCommentsByPost, updateCommentById, deleteCommentById } = require("../controllers/comment")
 const commentRouter=express.Router()
 
 commentRouter.post("/comment/:post_id",authentication,createNewComment)
 commentRouter.get("/comment/:post_id",authentication,getCommentsByPost)
 commentRouter.put("/comment/:comment_id",authentication,updateCommentById)
+commentRouter.delete("/comment/comment_id",authentication,deleteCommentById)
 module.exports=commentRouter
