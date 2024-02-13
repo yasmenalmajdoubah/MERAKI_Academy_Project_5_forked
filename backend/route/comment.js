53eb5edc76efb2b14748d97fb6ef953fb57e1a26
HamzaFAqel <hamzah.f.aqel@gmail.com>
2024-02-13 21:09:15 +0300

    update after test functions comment

--- backend/route/comment.js
+++ backend/route/comment.js
@@ -3,8 +3,15 @@ const authentication = require("../middlewares/authentication")
 const { createNewComment, getCommentsByPost, updateCommentById, deleteCommentById } = require("../controllers/comment")
 const commentRouter=express.Router()
 
+http://localhost:5000/comments/comment/3
 commentRouter.post("/comment/:post_id",authentication,createNewComment)
-commentRouter.get("/comment/:post_id",authentication,getCommentsByPost)
+
+//http://localhost:5000/comments/comment/3
+commentRouter.get("/comment/:post_id",getCommentsByPost)
+
+//http://localhost:5000/comments/comment/2
 commentRouter.put("/comment/:comment_id",authentication,updateCommentById)
-commentRouter.delete("/comment/comment_id",authentication,deleteCommentById)
+
+//http://localhost:5000/comments/comment/3
+commentRouter.delete("/comment/:comment_id",authentication,deleteCommentById)
 module.exports=commentRouter
