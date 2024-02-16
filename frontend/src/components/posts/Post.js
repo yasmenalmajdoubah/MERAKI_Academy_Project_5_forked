4ec900a11a1f2fcf9c944f90e86e2d916c91724c
HamzaFAqel <hamzah.f.aqel@gmail.com>
2024-02-16 17:02:05 +0300

    post Slice

---
+++ frontend/src/components/posts/Post.js
@@ -0,0 +1,33 @@
+import { useSelector,useDispatch } from "react-redux";
+import { useEffect } from "react";
+import React from 'react'
+import axios from "axios";
+import { allPost } from "../../service/redux/reducers/posts/postsSlice";
+
+export const Post = () => {
+    const dispatch=useDispatch()
+    const state=useSelector((state)=>{
+        return {
+         postURL:state.posts.postURL
+        }
+      })
+    useEffect(() => {
+      
+    axios.get(`${state.postURL}`,  {
+        headers: {
+          Authorization: `Bearer ${state.token}`,
+        },
+      }).then((result)=>{
+        dispatch(allPost(result.data.posts))
+      }).catch((err)=>{
+        console.log(err.message);
+      })
+      
+    }, [/* state.postURL */])
+    
+  return (
+    <div>
+
+    </div>
+  )
+}
