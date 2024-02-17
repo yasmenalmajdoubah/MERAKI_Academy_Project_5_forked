bc74c349de050083625c33a9a7ba1fb00447ce15
yasmenalmajdoubah <yasmen.almajdoubah@gmail.com>
2024-02-16 21:11:12 -0800

    login -> state

--- frontend/src/pages/Login/Login.js
+++ frontend/src/pages/Login/Login.js
@@ -1,8 +1,26 @@
 import React from 'react'
-
+import { useDispatch, useSelector } from "react-redux";
+import { setLogin, setUserId, setLogout } from "../redux/reducers/log";
 const Login = () => {
+
+  const dispatch = useDispatch();
+
+  const state = useSelector ((state)=>{
+    return {
+      token : state.log.token,
+      userId:state.log.userId,
+      
+    }
+   })
+
+
+
+
+
+
+
   return (
-    <div >Login</div>
+    <div  className='font-black' >Login</div>
   )
 }
 
