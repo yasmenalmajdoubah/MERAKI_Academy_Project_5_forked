468d13022229950c02d5a8c0b1d42da9820c1a8e
yasmenalmajdoubah <yasmen.almajdoubah@gmail.com>
2024-02-17 00:25:06 -0800

    register function

--- frontend/src/pages/Register/Register.js
+++ frontend/src/pages/Register/Register.js
@@ -1,6 +1,6 @@
 import React ,{  useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-//import { setLogin, setUserId, setLogout } from "../../service/redux/reducers/log/logSlice";
+
 import axios from "axios";
 const Register = () => {
   /*
@@ -19,7 +19,7 @@ const Register = () => {
     role_id,
   */
   
-  const dispatch = useDispatch();
+  //const dispatch = useDispatch();
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [jobName, setJobName] = useState("");
@@ -28,9 +28,108 @@ const Register = () => {
   const [password, setPassword] = useState("");
   const [phoneNumber, setPhoneNumber] = useState(0);
   const [about, setAbout] = useState("");
+  const [CV, setCV] = useState("");
+  const [profileImage, setProfileImage] = useState("");
+  const [coverImage, setCoverImage] = useState("");
+  ////
+  const [message, setMessage] = useState("");
+  const [status, setStatus] = useState(false);
+
+
+  const addNewUser = async (e) => {
+    e.preventDefault();
+    try {
+      const result = await axios.post("http://localhost:5000/users/register", {
+        firstName,
+        lastName,
+       // profileImage,
+       // coverImage,
+        jobName,
+        country,
+        email,
+        password,
+        about,
+      //  CV,
+        phoneNumber
+      });
+      if (result.data.success) {
+        setStatus(true);
+        setMessage(result.data.message);
+      } else throw Error;
+    } catch (error) {
+      setStatus(false);
+      if (error.response && error.response.data) {
+        return setMessage(error.response.data.message);
+      }
+      setMessage("Error happened while register, please try again");
+    }
+  };
+
   return (
-    <div>Register</div>
-  )
+    <>
+      <div className="Form">
+        
+          <>
+            <p className="Title">Register:</p>
+            <form onSubmit={addNewUser}>
+              <br />
+              <input
+                type="text"
+                placeholder="First Name"
+                onChange={(e) => setFirstName(e.target.value)}
+              />
+              <br />
+              <input
+                type="text"
+                placeholder="Last Name"
+                onChange={(e) => setLastName(e.target.value)}
+              />
+              <br />
+              <input
+                type="number"
+                placeholder="phoneNumber"
+                onChange={(e) => setPhoneNumber(e.target.value)}
+              />
+              <br />
+              <input
+                type="text"
+                placeholder="Country"
+                onChange={(e) => setCountry(e.target.value)}
+              />
+              <br />
+              <input
+                type="text"
+                placeholder="About"
+                onChange={(e) => setAbout(e.target.value)}
+              />
+              <br />
+              <input
+                type="text"
+                placeholder="Job Name"
+                onChange={(e) => setJobName(e.target.value)}
+              />
+              <br />
+              <input
+                type="email"
+                placeholder="Email"
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <br />
+              <input
+                type="password"
+                placeholder="Password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <br />
+              <button>Register</button>
+              <br />
+            </form>
+           
+          </>
+      
+      </div>
+    </>
+  );
 }
 
 export default Register
