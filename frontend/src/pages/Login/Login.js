8f0aecad1228827671d792786bdf0676e91d53ed
yasmenalmajdoubah <yasmen.almajdoubah@gmail.com>
2024-02-16 21:50:16 -0800

    login function + login retern

--- frontend/src/pages/Login/Login.js
+++ frontend/src/pages/Login/Login.js
@@ -1,6 +1,8 @@
 import React ,{  useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { setLogin, setUserId, setLogout } from "../../service/redux/reducers/log";
+import { setLogin, setUserId, setLogout } from "../../service/redux/reducers/log/logSlice";
+import axios from "axios";
+
 const Login = () => {
 
   const dispatch = useDispatch();
@@ -17,13 +19,65 @@ const Login = () => {
    const [message, setMessage] = useState("");
    const [password, setPassword] = useState("");
 
+   const login = async (e) => {
+    e.preventDefault();
+    try {
+      const result = await axios.post("http://localhost:5000/users/login", {
+        email,
+        password,
+      });
+      if (result.data) {
+       
+        dispatch(setLogin(result.data.token))
+        
+        dispatch(setUserId(result.data.userId))
+
+        
+      } else throw Error;
+    } catch (error) {
+      if (error.response && error.response.data) {
+        return setMessage(error.response.data.message);
+      }
+      setMessage("Error happened while Login, please try again");
+    }
+  };
 
 
 
 
-  return (
-    <div  className='font-black' >Login</div>
-  )
-}
+  
+    return (
+      <>
+        <div className="Form">
+          <p className="Title">Login:</p>
+          <form onSubmit={login}>
+            <br />
+  
+            <input
+              type="email"
+              placeholder="Email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <br />
+            <input
+              type="password"
+              placeholder="Password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <br />
+            <button
+              onClick={(e) => {
+                login(e);
+              }}
+            >
+              Login
+            </button>
+          </form>
+  
+          
+        </div>
+      </>
+    );
+  }
 
 export default Login
