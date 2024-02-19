088f9be909bb8f93ad118f59ffc4d6d66304446d
yasmenalmajdoubah <yasmen.almajdoubah@gmail.com>
2024-02-18 20:16:21 -0800

    Porfile post

--- frontend/src/pages/Profile/PorfilePosts.js
+++ frontend/src/pages/Profile/PorfilePosts.js
@@ -1,6 +1,37 @@
-import React from 'react'
-
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect, useState } from "react";
+import React from "react";
+import axios from "axios";
+import {
+  allPost,
+  allComments,
+  addComment,
+} from "../../service/redux/reducers/posts/postsSlice";
 const PorfilePosts = () => {
+
+
+
+  const [comment, setComment] = useState(""); //
+  const [message, setMessage] = useState("");
+  const [post_id, setPost_id] = useState(false);
+  const [show, setShow] = useState("");
+  const dispatch = useDispatch();
+  const state = useSelector((state) => {
+    return {
+      token: state.log.token,
+      postURL: state.posts.postURL,
+      posts: state.posts.posts,
+    };
+  });
+
+
+
+
+
+
+
+
+
   return (
     <div>PorfilePosts</div>
   )
