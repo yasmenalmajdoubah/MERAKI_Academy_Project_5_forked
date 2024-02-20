9fdcf6b4c318388e8fd26604c595a940e513f346
bakirhammad <bakir.hammad@gmail.com>
2024-02-20 14:51:16 +0300

    register5

--- frontend/src/pages/Profile/PorfilePosts.js
+++ frontend/src/pages/Profile/PorfilePosts.js
@@ -7,28 +7,28 @@ import {
   allComments,
   addComment,
 } from "../../service/redux/reducers/posts/postsSlice";
-const PorfilePosts = () => {
 
-
-
+export const PorfilePosts = () => {
   const [comment, setComment] = useState(""); //
   const [message, setMessage] = useState("");
   const [post_id, setPost_id] = useState(false);
   const [show, setShow] = useState("");
   const dispatch = useDispatch();
-  const {token,posts,userId} = useSelector((state) => {
+  const state = useSelector((state) => {
     return {
       token: state.log.token,
+      postURL: state.posts.postURL,
       posts: state.posts.posts,
-      userId: state.log.userId
+      userId: state.log.userId,
     };
   });
-  console.log(posts);
+  console.log(state.posts);
+  // ===============================
   useEffect(() => {
     axios
-      .get(`http://localhost:5000/posts/search_1?user=${userId}`, {
+      .get(`http://localhost:5000/posts/search_1?user=${state.userId}`, {
         headers: {
-          Authorization: `Bearer ${token}`,
+          Authorization: `Bearer ${state.token}`,
         },
       })
       .then((result) => {
@@ -38,18 +38,29 @@ const PorfilePosts = () => {
       .catch((err) => {
         console.log(err.message);
       });
-  console.log(posts);
-
   }, []);
 
+  /*
+  const handleUpdateClick = (post) => {
+    
+    setPost_id(post.post_id);
+    
+   
+
+  }
+
+*/
+  ///get comments
   const getCommentsByPost = async (post_id) => {
+    console.log(post_id);
     try {
       const result = await axios.get(
         `http://localhost:5000/comments/comment/${post_id}`
       );
       if (result.data.success) {
-        const comments = result.data.result;
-        console.log(result.data);
+        console.log("result.data", result.data);
+        const comments = result.data.comments;
+        console.log("comments", comments);
         dispatch(allComments({ comments, post_id }));
       } else throw Error;
     } catch (error) {
@@ -60,7 +71,6 @@ const PorfilePosts = () => {
     }
   };
 
-
   const createComment = async (post_id) => {
     try {
       const result = await axios.post(
@@ -70,103 +80,138 @@ const PorfilePosts = () => {
         },
         {
           headers: {
-            Authorization: `Bearer ${token}`,
+            Authorization: `Bearer ${state.token}`,
           },
         }
       );
       if (result.data.success) {
-        const newCommet = result.data.result
-        dispatch(addComment({ newCommet, post_id }))
-        getCommentsByPost(post_id);
+        console.log("result.data", result.data);
+        const newCommet = result.data.results;
+        console.log("123", newCommet);
+        dispatch(addComment({ newCommet, post_id }));
+        //getCommentsByPost(post_id);
       }
     } catch (error) {
       console.log(error);
     }
   };
 
-
-
-
+  // ================================
   return (
     <div>
       {/* <form className="posts-form"> */}
-      {posts?.map((post, index) => {
+      {state.posts?.map((post, index) => {
         return (
-          <div key={index} className="posts">
-            <div>{post.post_id}</div>
-            <div>{post.body}</div>
-            <div>{post.image}</div>
-            <div>
-              {!post.comments && (
-                <button
-                  className="ShowBtn"
-                  onClick={() => {
-                    getCommentsByPost(post.post_id);
-                    setShow(post.post_id);
-                  }}
-                >
-                  Comment
-                </button>
-              )}
-              <div>
+          <div key={post_id} className="bg-gray-100 relative">
+            <div className="container mx-auto p-9">
+              <div className="bg-white rounded-lg shadow p-4">
+                <div className="flex items-center">
+                  <img
+                    src={post.profileimage}
+                    alt="Profile Picture"
+                    className="w-12 h-12 rounded-full"
+                  />
+                  <div className="ml-2">
+                    <p className="font-semibold">
+                      {post.firstname} {post.lastname}
+                    </p>
+                    <p class="text-gray-500 text-xs">
+                      {" "}
+                      Posted{" "}
+                      {post.created_at
+                        .split("T")
+                        .shift()
+                        .split("-")
+                        .reverse()
+                        .join("-")}
+                    </p>
+                  </div>
+                </div>
+                <div className="mt-4">
+                  <p>{post.body}</p>
+                  {post.image && (
+                    <img src={post.image} alt="Post Image" className="mt-4" />
+                  )}
+                </div>
+                <div className="flex items-center justify-between mt-4">
+                  <button
+                    id="like-button"
+                    class="bg-gray-500 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded"
+                  >
+                    LIKE
+                  </button>
+                  <button
+                    id="comment-button"
+                    class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
+                    onClick={() => {
+                      console.log(post);
+                      getCommentsByPost(post.post_id);
+                      setShow(post.post_id);
+                    }}
+                  >
+                    COMMENT
+                  </button>
+                  {/* ========== */}
+                  <div className="bg-white absolute top-56 left-52 w-96 h-auto">
+                    <p className="text-center">Commets</p>
+                    <div class="mt-4">
+                      <textarea
+                        id="comment-textarea"
+                        class="text-center w-96 h-28 p-2 bg-gray-200 rounded"
+                        placeholder="WRITE YOUR COMMENT HERE"
+                        onChange={(e) => {
+                          setComment(e.target.value);
+                        }}
+                      ></textarea>
+                      <button
+                        id="post-comment-button"
+                        class="mt-2 bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
+                        onClick={() => {
+                          if (comment) createComment(post.post_id);
+                        }}
+                      >
+                        Add Comment
+                      </button>
+                    </div>
+                  </div>
+                </div>
                 {post.comments?.map((comment, i) => {
+                  console.log(comment);
                   return (
                     <p className="comment" key={i}>
                       {comment.comment}
                     </p>
                   );
                 })}
-              </div>
-
-              {show == post.id && (
-                <div>
-                  <textarea
-                    className="commentBox"
-                    placeholder="comment..."
-                    onChange={(e) => {
-                      setComment(e.target.value);
-                    }}
-                  />
-                  <button
-                    className="commentBtn"
-                    onClick={() => {
-                      if (comment) createComment(post.id);
-                    }}
-                  >
-                    Add comment
-                  </button>
+                <div class="comment-section mt-4">
+                  <div class="bg-white rounded-lg shadow p-4"></div>
                 </div>
-              )}
-
-              {show == post.post_id && (
-                <div>
+                <div class="mt-4">
                   <textarea
-                    className="commentBox"
-                    placeholder="comment..."
+                    id="comment-textarea"
+                    class="w-full h-32 p-2 bg-gray-200 rounded"
+                    placeholder="WRITE YOUR COMMENT HERE"
                     onChange={(e) => {
                       setComment(e.target.value);
                     }}
-                  />
+                  ></textarea>
                   <button
-                    className="commentBtn"
+                    id="post-comment-button"
+                    class="mt-2 bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
                     onClick={() => {
                       if (comment) createComment(post.post_id);
                     }}
                   >
-                    Add comment
+                    COMMENT
                   </button>
                 </div>
-              )}
-
-              <button onClick={() => {}}>LIKE</button>
+              </div>
             </div>
           </div>
         );
       })}
-
-      {/* </form> */}
     </div>
   );
 };
 
-export default PorfilePosts
+export default PorfilePosts;
