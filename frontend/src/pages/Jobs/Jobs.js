33e0e34e958f016deb9857305181ba59f1c25251
yasmenalmajdoubah <yasmen.almajdoubah@gmail.com>
2024-02-21 21:46:28 -0800

    job

--- frontend/src/pages/Jobs/Jobs.js
+++ frontend/src/pages/Jobs/Jobs.js
@@ -2,7 +2,36 @@ import React from 'react'
 
 const Jobs = () => {
   return (
-    <div>Jobs</div>
+    <div>
+         <div class="flex h-screen">
+        <div class="w-2/5 bg-gray-100">
+            <h>12233</h>
+        </div>
+        <div class="w-3/5 bg-gray-100">
+        <>
+       <div class="bg-white  shadow-md rounded-md p-6 mb-6 max-w-2xl">
+       <div class="container mx-auto p-9">
+              <div class="bg-white rounded-lg shadow p-4">
+                  <div class="flex items-center">
+                      <img src ="https://w0.peakpx.com/wallpaper/979/89/HD-wallpaper-purple-smile-design-eye-smily-profile-pic-face-thumbnail.jpg" alt="Profile Picture" class="w-12 h-12 rounded-full"/>
+                      <div class="ml-2">
+                          <p class="font-semibold">company name</p>
+                      </div>
+                      </div>
+                      </div>
+                      </div>
+        <h2 class="text-2xl font-bold mb-4"> JOB TITLE</h2>
+        <div class="bg-white shadow-md rounded-md p-6 mb-6 max-w-2xl"><p class="text-gray-600 text-lg mb-4"> aaaaaaaa</p></div>
+        <h2 class="text-2xl font-bold mb-4">  JOB DESCRIPITON </h2>
+        <div class="bg-white shadow-md rounded-md p-6 mb-6 max-w-2xl"><p class="text-gray-600 text-lg mb-4"> aaaaaaaa</p></div>
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          APPLY
+        </button>
+      </div>
+    </>
+        </div>
+    </div>
+    </div>
   )
 }
 
