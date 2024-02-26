3ea6296f30429ee396f152905dcc0f9b969609f3
bakirhammad <bakir.hammad@gmail.com>
2024-02-26 21:13:36 +0300

    chat2

--- backend/route/chat.js
+++ backend/route/chat.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const { createChat, getAllMessages } = require("../controllers/chat");
-const { authentication } = require("../middlewares/authentication");
+const authentication = require("../middlewares/authentication");
 
 const chatRouter = express.Router();
 
+// http://localhost:5000/chat/create
 chatRouter.post("/create", authentication, createChat);
-chatRouter.get("/messages", authentication, getAllMessages);
+
+// http://localhost:5000/chat/messages/5
+chatRouter.get("/messages/:to", authentication, getAllMessages);
 
 module.exports = chatRouter;
