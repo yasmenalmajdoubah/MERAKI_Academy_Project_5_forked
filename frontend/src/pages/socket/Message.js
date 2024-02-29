3968587e8912c2ebb75bc49879fd26640f1fe6ba
yasmenalmajdoubah <yasmen.almajdoubah@gmail.com>
2024-02-29 11:40:04 -0800

    mess

--- frontend/src/pages/socket/Message.js
+++ frontend/src/pages/socket/Message.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router";
+import axios from "axios";
 
 import "./style.css";
 
@@ -8,18 +9,44 @@ import { useSelector } from "react-redux";
 const Message = ({socket,toId}) => {
     const [messages, setMessages] = useState("");
     const [allMessage, setAllMessage] = useState([]);
-    const { userId } = useSelector((state) => {
+    const [to, setTo] = useState({});
+
+    const { userId,token } = useSelector((state) => {
         return {
          
           userId: state.log.userId,
-          
+          token: state.log.token,
     
         };
       });
 
 
+      const getUser = () => {
+        axios
+          .get(`http://localhost:5000/users/search_1/${toId}`, {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          })
+          .then((result) => {
+            console.log('result', result.data.result)
+           setTo(result.data.result);
+           console.log ("to",to)
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      };
+
+
+
+
+
+
+
 
     useEffect(() => {
+        getUser()
         // add a an event listener on message events
         socket.on("message", reciveData);
         // remove all listeners on clean up
